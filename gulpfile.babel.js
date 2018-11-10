71b27422015735d77ef2c9f394922b1abc1d23d2
Sahin Deniz <seahindeniz@gmail.com>
2018-11-10 03:36:21 +0300

    Dependency cleaning

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,7 @@
 import fs from "fs";
-import gulp from 'gulp';
-import { merge } from 'event-stream'
+import { task, src, dest, series, parallel, watch } from 'gulp';
 import browserify from 'browserify';
 import babelify from 'babelify';
-import source from 'vinyl-source-stream';
-import buffer from 'vinyl-buffer';
-import preprocessify from 'preprocessify';
-import gulpif from "gulp-if";
-import bro from "gulp-bro";
-import yaml from "gulp-yaml";
 
 const $ = require('gulp-load-plugins')();
 
@@ -44,50 +37,40 @@ var manifest = {
 		}
 	}
 }
+
 // Tasks
-gulp.task('clean', () => {
-	return pipe(`./build/${target}`, $.clean())
+task('clean', () => {
+	return src(`./build/${target}`)
+		.pipe($.clean());
 })
 
 // -----------------
 // COMMON
 // -----------------
-gulp.task('js', () => {
-	return gulp.src([
+task('js', () => {
+	return src([
 			'src/scripts/*.js',
 			'src/scripts/**/**/*.js',
 			'!src/scripts/utils/*.js',
 			"!src/scripts/locales/**/*.js",
 			"!src/scripts/lib/*.min.js"
 		])
-		.pipe(bro({
+		.pipe($.bro({
 			transform: [
 				babelify.configure({ presets: ['latest'] }),
 				['uglifyify', { global: true }]
 			]
 		}))
-		.pipe(gulp.dest(`build/${target}/scripts`));
+		.pipe(dest(`build/${target}/scripts`));
 });
-gulp.task('js-min', () => {
-	return gulp.src([
-			"src/scripts/**/*.min.js"
-		])
-		.pipe(gulp.dest(`build/${target}/scripts`));
-});
-gulp.task("locales", () => {
-	return gulp.src([
-			'src/config/*.json'
-		])
-		.pipe(gulp.dest(`build/${target}/config`));
-});
-gulp.task("js-config", () => {
-	return gulp.src('src/config/locales/*.yml')
-		.pipe(yaml())
-		.pipe(gulp.dest(`build/${target}/config/locales`));
+task("locales", () => {
+	return src('src/config/locales/*.yml')
+		.pipe($.yaml())
+		.pipe(dest(`build/${target}/config/locales`));
 });
 
-gulp.task('styles', () => {
-	return gulp.src([
+task('styles', () => {
+	return src([
 			'src/styles/**/*.scss',
 		])
 		.pipe($.plumber())
@@ -98,10 +81,10 @@ gulp.task('styles', () => {
 			includePaths: ['.']
 		}).on('error', $.sass.logError))
 		.pipe($.sourcemaps.write(`./`))
-		.pipe(gulp.dest(`build/${target}/styles`));
+		.pipe(dest(`build/${target}/styles`));
 });
-gulp.task('views_styles', () => {
-	return gulp.src('src/scripts/views/**/*.scss')
+task('styles_views', () => {
+	return src('src/scripts/views/**/*.scss')
 		.pipe($.plumber())
 		.pipe($.sourcemaps.init())
 		.pipe($.sass.sync({
@@ -110,30 +93,42 @@ gulp.task('views_styles', () => {
 			includePaths: ['.']
 		}).on('error', $.sass.logError))
 		.pipe($.sourcemaps.write(`./`))
-		.pipe(gulp.dest(`build/${target}/scripts/views`));
+		.pipe(dest(`build/${target}/scripts/views`));
 });
 
-gulp.task("manifest", () => {
-	return gulp.src('./manifest.json')
-		.pipe(gulpif(!production, $.mergeJson({
+task("manifest", () => {
+	return src('./manifest.json')
+		.pipe($.if(!production, $.mergeJson({
 			fileName: "manifest.json",
 			jsonSpace: " ".repeat(4),
 			endObj: manifest.dev
 		})))
-		.pipe(gulpif(production, $.mergeJson({
+		.pipe($.if(production, $.mergeJson({
 			fileName: "manifest.json",
 			jsonSpace: " ".repeat(4),
 			endObj: manifest.production
 		})))
-		.pipe(gulpif(target === "firefox", $.mergeJson({
+		.pipe($.if(target === "firefox", $.mergeJson({
 			fileName: "manifest.json",
 			jsonSpace: " ".repeat(4),
 			endObj: manifest.firefox
 		})))
-		.pipe(gulp.dest(`./build/${target}`))
+		.pipe(dest(`./build/${target}`))
 });
-gulp.task("mergeAll", (cb) => {
-	let toMerge = [{
+/*task('js-min', () => {
+	return src([
+			"src/scripts/** /*.min.js"
+		])
+		.pipe(dest(`build/${target}/scripts`));
+});
+task("market-configs", () => {
+	return src([
+			'src/config/*.json'
+		])
+		.pipe(dest(`build/${target}/config`));
+});*/
+task("assets", (cb) => {
+	let assets = [{
 			src: './src/icons/**/*',
 			dest: `./build/${target}/icons`
 		},
@@ -152,105 +147,71 @@ gulp.task("mergeAll", (cb) => {
 		{
 			src: './src/**/*.html',
 			dest: `./build/${target}`
+		},
+		{
+			src: "src/scripts/**/*.min.js",
+			dest: `build/${target}/scripts`
+		},
+		{
+			src: "src/config/*.json",
+			dest: `build/${target}/config`
 		}
 	];
 
-	console.log(toMerge);
-	toMerge = toMerge.map(function(folder, i) {
-		let _gulp = gulp.src(folder.src)
-			.pipe(gulp.dest(folder.dest));
-		if (toMerge.length - 1 == i) {
-			//_gulp.on("end", cb);
-		}
-		return _gulp;
+	assets = assets.map(function(asset) {
+		return src(asset.src)
+			.pipe(dest(asset.dest));
 	});
-	//mergeAll(target);
-	//cb();
-	return toMerge[toMerge.length - 1]
+
+	return assets[assets.length - 1]
 })
-gulp.task('ext', gulp.series('manifest', "mergeAll", 'js', 'js-min', "js-config", "locales"));
+task(
+	'ext',
+	series('manifest', 'js', "locales", "assets"));
 
-/* gulp.task('build', (cb) => {
-	$.runSequence('clean', 'styles', 'views_styles', 'ext', cb)
-}); */
-gulp.task(
+task(
 	'build',
-	gulp.series('clean', 'styles', 'views_styles', 'ext')
+	series('clean', 'styles', 'styles_views', 'ext')
 );
-gulp.task("reloadExtension", callback => {
-	$.livereload.reload();
-	callback();
-});
-gulp.task("watchFiles", () => {
-	$.livereload.listen();
+task(
+	"reloadExtension",
+	callback => {
+		$.livereload.reload();
+		callback();
+	}
+);
+task(
+	"watchFiles",
+	() => {
+		$.livereload.listen();
 
-	return gulp.watch('./src/**/*', gulp.series('build', "reloadExtension"));
-});
+		return watch('./src/**/*', series('build', "reloadExtension"));
+	}
+);
 
-gulp.task('watch', gulp.series('build', "watchFiles"));
+task(
+	'watch',
+	series('build', "watchFiles")
+);
 
-gulp.task('default', gulp.series('build'));
+task(
+	'default',
+	series('build')
+);
+
 // -----------------
 // DIST
 // -----------------
-/*gulp.task('dist', (cb) => {
-	$.runSequence('build', 'zip', cb)
-});*/
-gulp.task('zip', () => {
-	return pipe(`./build/${target}/**/*`, $.zip(`${target}-${process.env.npm_package_version}.zip`), '../dists')
-})
-gulp.task('dist', gulp.series('build', 'zip'));
+task(
+	'zip',
+	() => {
+		return src(`./build/${target}/**/*`)
+			.pipe($.zip(`${target}-${process.env.npm_package_version}.zip`))
+			.pipe(dest('../dists'))
+	}
+);
 
-// Helpers
-function pipe(src, ...transforms) {
-	return transforms.reduce((stream, transform) => {
-		const isDest = typeof transform === 'string'
-		return stream.pipe(isDest ? gulp.dest(transform) : transform)
-	}, gulp.src(src))
-}
-
-function mergeAll(dest) {
-	return merge(
-		pipe('./src/icons/**/*', `./build/${dest}/icons`),
-		pipe(['./src/_locales/**/*'], `./build/${dest}/_locales`),
-		pipe([`./src/images/${target}/**/*`], `./build/${dest}/images`),
-		pipe(['./src/images/shared/**/*'], `./build/${dest}/images`),
-		pipe(['./src/**/*.html'], `./build/${dest}`)
-	)
-}
-
-function buildJS(target) {
-	const files = [
-		'background.js',
-		'contentscript.js',
-		'options.js',
-		'popup.js',
-		'livereload.js'
-	]
-
-	let tasks = files.map(file => {
-		return browserify({
-				entries: 'src/scripts/' + file,
-				debug: true
-			})
-			.transform('babelify', { presets: ['es2015'] })
-			.transform(preprocessify, {
-				includeExtensions: ['.js'],
-				context: context
-			})
-			.bundle()
-			.pipe(source(file))
-			.pipe(buffer())
-			.pipe(gulpif(!production, $.sourcemaps.init({ loadMaps: true })))
-			.pipe(gulpif(!production, $.sourcemaps.write('./')))
-			.pipe(gulpif(production, $.uglify({
-				"mangle": false,
-				"output": {
-					"ascii_only": true
-				}
-			})))
-			.pipe(gulp.dest(`build/${target}/scripts`));
-	});
-
-	return merge.apply(null, tasks);
-}
+task(
+	'dist',
+	series('build', 'zip')
+);

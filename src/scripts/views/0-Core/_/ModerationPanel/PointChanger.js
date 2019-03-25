ddccb272204330f27979d862f4f6ea598944faa2
Sahin Deniz <seahindeniz@gmail.com>
2019-03-25 21:14:47 +0300

    resolves #42

--- src/scripts/views/0-Core/_/ModerationPanel/PointChanger.js
+++ src/scripts/views/0-Core/_/ModerationPanel/PointChanger.js
@@ -6,6 +6,7 @@ const spinner = `<div class="sg-spinner-container__overlay"><div class="sg-spinn
 class PointChanger {
 	constructor() {
 		this.users = [];
+		this.usersWithPoints = {};
 
 		this.Init();
 	}
@@ -38,7 +39,8 @@ class PointChanger {
 						<div class="sg-content-box__content sg-content-box__content--with-centered-text sg-content-box__content--spaced-top">
 							<div class="sg-actions-list sg-actions-list--no-wrap">
 								<div class="sg-actions-list__hole sg-actions-list__hole--grow">
-									<input type="text" class="sg-input sg-input--full-width" placeholder="${System.data.locale.common.profileID}">
+									<input type="text" class="sg-input sg-input--full-width js-hidden" placeholder="${System.data.locale.common.profileID}">
+									<textarea class="sg-textarea sg-textarea--full-width sg-textarea--resizable-vertical" placeholder="${System.data.locale.common.profileID}"></textarea>
 								</div>
 								<div class="sg-actions-list__hole">
 									<button class="sg-button-secondary sg-button-secondary--alt">
@@ -70,12 +72,24 @@ class PointChanger {
 					</div>
 				</div>
 				<div class="sg-content-box__content">
-					<blockquote class="sg-text sg-text--small">${System.data.locale.core.pointChanger.youNeedToEnterOrPaste}</blockquote>
+					<blockquote class="sg-text sg-text--small">
+						${System.data.locale.core.pointChanger.youNeedToEnterOrPaste}<br>
+						${System.data.locale.core.pointChanger.pastingExample}<br>
+						${System.createProfileLink(System.data.Brainly.userData.user.nick,System.data.Brainly.userData.user.id)}<br>
+						${System.createProfileLink("Sakura", 314651)}<br>
+						1234567<br>
+						2345678<br><br>
+						${System.data.locale.core.pointChanger.pastingExample2}<br>
+						${System.createProfileLink(System.data.Brainly.userData.user.nick,System.data.Brainly.userData.user.id)} +1000<br>
+						${System.createProfileLink("Sakura", 314651)} -200<br>
+						1234567 -95<br>
+						2345678 +645<br>
+					</blockquote>
 				</div>
 			</div>`,
 			size: "medium"
 		});
-		this.$idInput = $(".id input", this.modal.$modal);
+		this.$idInput = $(".id textarea", this.modal.$modal);
 		this.$userList = $(".js-user-list", this.modal.$modal);
 		this.$close = $(".sg-toplayer__close", this.modal.$modal);
 		this.$idInputContainer = $(".sg-content-box.id", this.modal.$modal);
@@ -89,13 +103,12 @@ class PointChanger {
 		this.$close.click(this.modal.Close.bind(this.modal));
 		this.$li.on("click", "span", this.modal.Open.bind(this.modal));
 		this.$addPointToAllButton.click(this.AddPointToAll.bind(this));
+		this.$idInput.on("paste", this.IdInputPasteEvtHandle.bind(this));
 
 		this.$addUserButton.click(() => {
-			let id = this.GetID()
+			let idList = this.GetID()
 
-			if (this.IsNotFetched(id)) {
-				this.FindID(id);
-			}
+			this.FindIDs(idList);
 		});
 
 		this.$userList.on("click", `button.js-add-point`, async function() {
@@ -133,16 +146,52 @@ class PointChanger {
 				this.classList.add("sg-input--valid");
 			}
 		})
+	}
+	AddPointToAll() {
+		let $buttons = $(`button.js-add-point`, this.$userList);
 
-		this.$idInput.on("paste", e => {
-			e.preventDefault();
+		$buttons.click();
+	}
+	IdInputPasteEvtHandle(event) {
+		event.preventDefault();
 
-			let text = (e.originalEvent || e).clipboardData.getData("text/plain");
-			let idList = System.ExtractIds(text);
+		/**
+		 * @type {string}
+		 */
+		let saltText = (event.originalEvent || event).clipboardData.getData("text/plain");
 
-			if (!idList || idList.length == 0) {
-				document.execCommand("insertText", false, text);
-			} else {
+		if (saltText) {
+			let texts = saltText.split(/\r\n|\n/);
+			let idList = [];
+
+			if (texts && texts.length > 0) {
+				texts.forEach(text => {
+					let splittedText = text.trim().split(" ");
+
+					if (splittedText.length == 0 || !splittedText[0]) {
+						if (text)
+							this.Paste(`${text}\n`);
+					} else {
+						let id = System.ExtractId(splittedText[0]);
+
+						if (!id) {
+							this.Paste(`${splittedText[0]}\n`);
+						} else {
+							idList.push(id);
+
+							if (splittedText.length > 1) {
+								let points = splittedText[1];
+
+								if (points[0] == "+" || points[0] == "-") {
+									this.usersWithPoints[id] = points.replace("+", "");
+								} else {
+									idList = [...idList, ...System.ExtractIds(splittedText)];
+								}
+							}
+						}
+					}
+				});
+
 				idList = Array.from(new Set(idList));
 				idList = this.FilterFetchedUsers(idList);
 
@@ -152,22 +201,19 @@ class PointChanger {
 					this.FindIDs(idList);
 				}
 			}
-		});
+		}
 	}
-	AddPointToAll() {
-		let $buttons = $(`button.js-add-point`, this.$userList);
-
-		$buttons.click();
+	Paste(text = "") {
+		document.execCommand("insertText", false, text);
 	}
 	GetID() {
 		let value = this.$idInput.val();
 
 		if (value) {
-			let id = System.ExtractId(value);
+			let idList = System.ExtractIds(value.split(" "));
 
-			if (id) {
-				return id;
-			}
+			if (idList)
+				return idList;
 		}
 	}
 	/**
@@ -180,29 +226,30 @@ class PointChanger {
 	 * @param {number[]} idList
 	 */
 	FilterFetchedUsers(idList) {
-		return idList.reduce((acc, id) => {
-			if (this.IsNotFetched(id))
-				acc.push(id);
-
-			return acc;
-		}, []);
+		return idList.filter(this.IsNotFetched.bind(this));
 	}
 	/**
 	 * @param {number[]} idList
 	 */
 	async FindIDs(idList) {
-		let res = await GetUsersByID(idList);
+		//idList = idList.filter(this.IsNotFetched.bind(this));
+		idList = this.FilterFetchedUsers(idList);
 
-		if (!res || !res.success) {
-			this.modal.notification(res.message || System.data.locale.common.notificationMessages.somethingWentWrong, "error");
-		} else {
-			res.data.forEach(this.AddUser.bind(this));
+		if (idList.length > 0) {
+			let res = await GetUsersByID(idList);
+
+			if (!res || !res.success) {
+				this.modal.notification(res.message || System.data.locale.common.notificationMessages.somethingWentWrong, "error");
+			} else {
+				this.ClearIdInput();
+				res.data.forEach(this.AddUser.bind(this));
+			}
 		}
 	}
 	async FindID(id) {
 		let res = await GetUserByID(id);
 
-		if (res || !res.success) {
+		if (!res || !res.success) {
 			this.modal.notification(res.message || System.data.locale.common.notificationMessages.somethingWentWrong, "error");
 		} else {
 			this.AddUser(res.data);
@@ -215,6 +262,8 @@ class PointChanger {
 	AddUser(user) {
 		let avatar = System.prepareAvatar(user);
 		let profileLink = System.createProfileLink(user.nick, user.id);
+		let $lastAddedUser = $(".js-node:last", this.$userList);
+		let preDefinedPoints = this.usersWithPoints[user.id];
 		let $node = $(`
 		<div class="sg-content-box sg-content-box--full js-node">
 			<div class="sg-content-box__content sg-content-box__content--spaced-top-small">
@@ -234,7 +283,7 @@ class PointChanger {
 					</div>
 					<div class="sg-actions-list__hole">
 						<span class="sg-text sg-text--small sg-text--gray sg-text--bold js-points">${user.points} + </span>
-						<input type="number" class="sg-input sg-input--small" placeholder="${System.data.locale.common.shortPoints}..">
+						<input type="number" class="sg-input sg-input--small" placeholder="${System.data.locale.common.shortPoints}.." tabindex="${$lastAddedUser.index() + 2}">
 						<div class="sg-spinner-container">
 							<button class="sg-button-secondary sg-button-secondary--small js-add-point" title="${System.data.locale.core.pointChanger.addPoint}">
 								<div class="sg-icon sg-icon--adaptive sg-icon--x14">
@@ -248,11 +297,18 @@ class PointChanger {
 			</div>
 		</div>`);
 
+		let $ptsInput = $(`input[type="number"]`, $node);
+
 		this.users.push(user.id);
 		this.ChangeAmountOfUser(1);
 		$node.prop("userData", user);
 		$node.insertBefore(this.$idInputContainer);
 		this.$addPointToAllButton.removeClass("js-hidden");
+
+		if (preDefinedPoints)
+			$ptsInput
+			.val(preDefinedPoints)
+			.trigger("input");
 	}
 	ChangeAmountOfUser(amount) {
 		let currentAmount = this.$amountOfUsers.text();

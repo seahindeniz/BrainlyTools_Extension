5a60f8d96bba001d4a5369e534f5bf2845613d9a
Sahin Deniz <seahindeniz@gmail.com>
2019-03-30 12:53:31 +0300

    Todays actions enhanced

--- src/scripts/views/1-Home/_/TodaysActions.js
+++ src/scripts/views/1-Home/_/TodaysActions.js
@@ -3,11 +3,13 @@ import WaitForElement from "../../../helpers/WaitForElement";
 async function TodaysActions() {
 	let infoBox = await WaitForElement(selectors.userInfoBoxPoints);
 
-	if (System.data.Brainly.userData.user.mod_actions_count) {
+	if (System.data.Brainly.userData.user.mod_actions_count >= 0) {
 		let $todaysActions = $(`
 		<div style="margin: -4px 0 3px;">
-			<span class="sg-text sg-text--xsmall sg-text--gray sg-text--capitalize">${System.data.locale.home.todaysActions}: </span>
-			<span class="sg-text sg-text--xsmall sg-text--gray sg-text--emphasised">${System.data.Brainly.userData.user.mod_actions_count}</span>
+			<a href="/moderation_new/view_moderator/${System.data.Brainly.userData.user.id}" target="_blank">
+				<span class="sg-text sg-text--xsmall sg-text--gray sg-text--capitalize">${System.data.locale.home.todaysActions}: </span>
+				<span class="sg-text sg-text--xsmall sg-text--gray sg-text--emphasised">${System.data.Brainly.userData.user.mod_actions_count}</span>
+			</a>
 		</div>`);
 
 		$todaysActions.insertBefore(infoBox);

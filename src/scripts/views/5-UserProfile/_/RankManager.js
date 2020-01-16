52eebe4a454be397c9c78d81844c0e8b2b91aee8
Sahin Deniz <seahindeniz@gmail.com>
2020-01-16 16:42:18 +0300

    Fix checkbox rendering

--- src/scripts/views/5-UserProfile/_/RankManager.js
+++ src/scripts/views/5-UserProfile/_/RankManager.js
@@ -1,8 +1,10 @@
+import { Checkbox, Flex, Text } from "@/scripts/components/style-guide";
+import Build from "@/scripts/helpers/Build";
+import Button from "../../../components/Button";
 import notification from "../../../components/notification";
 import Progress from "../../../components/Progress";
 import Action from "../../../controllers/Req/Brainly/Action";
 import WaitForElement from "../../../helpers/WaitForElement";
-import Button from "../../../components/Button";
 
 class RankManager {
   constructor(user) {
@@ -11,7 +13,8 @@ class RankManager {
     this.Find_DeleteRanksForm();
   }
   async Find_DeleteRanksForm() {
-    this.deleteAllRanksForm = await WaitForElement(`[action="/ranks/delete_user_special_ranks"]`, 1, true);
+    this.deleteAllRanksForm = await WaitForElement(
+      `[action="/ranks/delete_user_special_ranks"]`, 1, true);
     this.deleteAllRanksLi = $(this.deleteAllRanksForm).parent();
 
     this.Init();
@@ -28,7 +31,9 @@ class RankManager {
   }
   RenderLink() {
     this.$manageLi = $(`<li></li>`);
-    this.$manageLink = $(`<a href="#"><font color="red">${System.constants.config.reasonSign}</font> ${System.data.locale.userProfile.rankManager.title}</a>`);
+    this.$manageLink = $(
+      `<a href="#"><font color="red">${System.constants.config.reasonSign}</font> ${System.data.locale.userProfile.rankManager.title}</a>`
+    );
 
     this.$manageLink.appendTo(this.$manageLi);
     this.$manageLi.insertBefore(this.deleteAllRanksLi);
@@ -75,30 +80,57 @@ class RankManager {
 		</div>`);
   }
   RenderRanks() {
-    System.data.Brainly.defaultConfig.config.data.ranks.forEach(rank => {
+    System.data.Brainly.defaultConfig.config.data.ranks.forEach((rank, i) => {
       let userHasRank = this.user.ranks_ids.includes(rank.id);
 
       if (rank && rank.type == 5) {
-        let checkbox = $(`
-				<div class="sg-label sg-label--secondary">
-					<div class="sg-label__icon">
-						<div class="sg-checkbox">
-							<input type="checkbox" class="sg-checkbox__element" id="p-${rank.id}"${userHasRank ? " checked" : ""}>
-							<label class="sg-checkbox__ghost" for="p-${rank.id}">
-								<div class="sg-icon sg-icon--adaptive sg-icon--x10">
-									<svg class="sg-icon__svg">
-										<use xlink:href="#icon-check"></use>
-									</svg></div>
-							</label>
-						</div>
-					</div>
-					<label class="sg-label__text" style="color:#${rank.color || "000"};" for="p-${rank.id}" title="${rank.description}">
-						<a href="/admin/ranks/edit/${rank.id}" target="_blank">${rank.name}</a>
-						${rank.name != rank.description ? `<br><i class="sg-text sg-text--xsmall sg-text--gray">${rank.description}</i>` : ""}
-					</label>
-				</div>`);
+        let checkboxContainer = Build(Flex({
+          wrap: false,
+          tag: "label",
+          marginBottom: (
+            i + 1 !== System.data.Brainly.defaultConfig.config
+            .data.ranks.length ? "s" : ""
+          ),
+        }), [
+          [
+            Flex({ marginRight: "s" }),
+            Checkbox({
+              id: `p-${rank.id}`,
+              checked: userHasRank
+            })
+          ],
+          [
+            Flex({ direction: "column", alignItems: "flex-start" }),
+            [
+              [
+                Flex({ fullWidth: true, direction: "column" }),
+                Text({
+                  html: rank.name,
+                  weight: "bold",
+                  color: "blue-dark",
+                  href: `/admin/ranks/edit/${rank.id}`,
+                  size: "xsmall",
+                  title: rank.description,
+                  target: "_blank",
+                  /* style: {
+                    color: `#${rank.color}`
+                  } */
+                })
+              ],
+              rank.name != rank.description && [
+                Flex({ fullWidth: true, direction: "column" }),
+                Text({
+                  tag: "i",
+                  size: "xsmall",
+                  color: "gray",
+                  html: rank.description
+                })
+              ],
+            ]
+          ]
+        ]);
 
-        checkbox.appendTo(this.$rankContainer);
+        this.$rankContainer.append(checkboxContainer);
       }
     });
   }
@@ -131,11 +163,15 @@ class RankManager {
     this.$panel.appendTo(this.$manageLi);
   }
   async SaveSelectedRank() {
-    let $selectedRanks = $(`input[type="checkbox"]:checked:not(:disabled)`, this.$rankContainer);
+    let $selectedRanks = $(`input[type="checkbox"]:checked:not(:disabled)`,
+      this.$rankContainer);
     let tokens = {
-      key: $(`input[name="data[_Token][key]"]`, this.deleteAllRanksLi).val(),
-      fields: $(`input[name="data[_Token][fields]"]`, this.deleteAllRanksLi).val(),
-      lock: $(`input[name="data[_Token][lock]"]`, this.deleteAllRanksLi).val()
+      key: $(`input[name="data[_Token][key]"]`, this.deleteAllRanksLi)
+        .val(),
+      fields: $(`input[name="data[_Token][fields]"]`, this.deleteAllRanksLi)
+        .val(),
+      lock: $(`input[name="data[_Token][lock]"]`, this.deleteAllRanksLi)
+        .val()
     }
 
     this.ShowSpinner();
@@ -143,14 +179,17 @@ class RankManager {
     if (confirm(System.data.locale.common.notificationMessages.areYouSure)) {
       this.progress.$container.appendTo(this.$progressHole);
 
-      let removeAllRanksXHR = await new Action().RemoveAllRanks(window.profileData.id, tokens);
+      let removeAllRanksXHR = await new Action().RemoveAllRanks(window
+        .profileData.id, tokens);
       let redirectedUserID = System.ExtractId(removeAllRanksXHR.url);
 
       if (redirectedUserID != profileData.id) {
-        notification(System.data.locale.common.notificationMessages.somethingWentWrongPleaseRefresh, "error");
+        notification(System.data.locale.common.notificationMessages
+          .somethingWentWrongPleaseRefresh, "error");
       } else {
         this.progress.update(1);
-        this.progress.UpdateLabel(System.data.locale.userProfile.rankManager.allRanksDeleted);
+        this.progress.UpdateLabel(System.data.locale.userProfile.rankManager
+          .allRanksDeleted);
 
         if ($selectedRanks.length == 0) {
           this.progress.update(2);
@@ -159,16 +198,20 @@ class RankManager {
           this.progress.setMax($selectedRanks.length + 2);
           await System.Delay(500);
           this.progress.update(2);
-          this.progress.UpdateLabel(System.data.locale.userProfile.rankManager.updatingRanks);
+          this.progress.UpdateLabel(System.data.locale.userProfile.rankManager
+            .updatingRanks);
           await System.Delay(500);
 
           $selectedRanks.each(async (i, rankCheckbox) => {
             let rankId = rankCheckbox.id.replace("p-", "");
-            let rank = System.data.Brainly.defaultConfig.config.data.ranksWithId[rankId];
+            let rank = System.data.Brainly.defaultConfig.config.data
+              .ranksWithId[rankId];
             await new Action().AddRank(window.profileData.id, rankId);
 
             this.progress.update(i + 3);
-            this.progress.UpdateLabel(System.data.locale.userProfile.rankManager.xHasAssigned.replace("%{rank_name}", ` ${rank.name} `));
+            this.progress.UpdateLabel(System.data.locale.userProfile
+              .rankManager.xHasAssigned.replace("%{rank_name}",
+                ` ${rank.name} `));
 
             if ($selectedRanks.length == i + 1) {
               await System.Delay(500);
@@ -182,7 +225,8 @@ class RankManager {
   SavingCompleted() {
     this.HideSpinner();
     this.progress.UpdateLabel(System.data.locale.common.allDone);
-    notification(System.data.locale.userProfile.notificationMessages.afterSavingCompletedIgnoreNotifications, "success");
+    notification(System.data.locale.userProfile.notificationMessages
+      .afterSavingCompletedIgnoreNotifications, "success");
   }
   ShowSpinner() {
     this.$spinner.appendTo(this.$saveButtonContainer);

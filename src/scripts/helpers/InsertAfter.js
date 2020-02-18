3809cf1acc7fdc38439ab37c9234ab79a849b257
Sahin Deniz <seahindeniz@gmail.com>
2020-02-18 20:46:40 +0300

    Element type support

--- src/scripts/helpers/InsertAfter.js
+++ src/scripts/helpers/InsertAfter.js
@@ -1,6 +1,6 @@
 /**
- * @param {HTMLElement} newNode
- * @param {HTMLElement} referenceNode
+ * @param {Element | HTMLElement} newNode
+ * @param {Element | HTMLElement} referenceNode
  */
 export default function InsertAfter(newNode, referenceNode) {
   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);

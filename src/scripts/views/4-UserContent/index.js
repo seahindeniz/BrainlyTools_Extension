1ef8b27f081193b02d5649ede5a0c76fcba6733d
seahindeniz <seahindeniz@gmail.com>
2020-07-16 15:53:46 +0300

    resolves #149

--- src/scripts/views/4-UserContent/index.js
+++ src/scripts/views/4-UserContent/index.js
@@ -1,3 +1,4 @@
+import { Button, Flex, Icon } from "@style-guide";
 import InjectToDOM from "../../helpers/InjectToDOM";
 
 (() => {
@@ -11,4 +12,63 @@ import InjectToDOM from "../../helpers/InjectToDOM";
   if (System.checkRoute(4, "comments_tr")) type = "Comments";
 
   if (type) InjectToDOM(`/scripts/views/4-UserContent/${type}.js`);
+
+  const contentOld = document.querySelector("#content-old");
+
+  if (!contentOld) return;
+
+  const selectedPageLink = contentOld.querySelector(
+    `:scope > div > p > a[style^="color"]`,
+  );
+
+  if (!selectedPageLink) return;
+
+  const nextPageLink = selectedPageLink.nextElementSibling;
+  const previousPageLink = selectedPageLink.previousElementSibling;
+
+  if (!nextPageLink && !previousPageLink) return;
+
+  const nextPageButtonContainer = Flex();
+  const previousPageButtonContainer = Flex();
+  const buttonContainer = Flex({
+    justifyContent: "space-between",
+    className: "page-buttons",
+    children: [previousPageButtonContainer, nextPageButtonContainer],
+  });
+
+  contentOld.append(buttonContainer);
+
+  if (previousPageLink instanceof HTMLAnchorElement) {
+    const previousPageButton = new Button({
+      size: "xl",
+      iconOnly: true,
+      type: "solid-blue",
+      icon: new Icon({
+        type: "arrow_left",
+      }),
+    });
+
+    previousPageButtonContainer.append(previousPageButton.element);
+
+    previousPageButton.element.addEventListener("click", () => {
+      previousPageLink.click();
+    });
+  }
+
+  if (nextPageLink instanceof HTMLAnchorElement) {
+    const nextPageButton = new Button({
+      size: "xl",
+      iconOnly: true,
+      type: "solid-blue",
+      icon: new Icon({
+        type: "arrow_right",
+      }),
+    });
+
+    nextPageButtonContainer.append(nextPageButton.element);
+
+    nextPageButton.element.addEventListener("click", () => {
+      nextPageLink.click();
+    });
+  }
 })();

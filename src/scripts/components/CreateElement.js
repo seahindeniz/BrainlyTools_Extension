ab05a46cf1ed562999044a25de7978104919b31b
seahindeniz <seahindeniz@gmail.com>
2020-07-20 19:05:39 +0300

    Add more types

--- src/scripts/components/CreateElement.js
+++ src/scripts/components/CreateElement.js
@@ -1,40 +1,41 @@
-import classnames from 'classnames';
-import AddChildren from '@style-guide/helpers/AddChildren';
-import SetProps from '@style-guide/helpers/SetProps';
+// @flow strict
 
-/**
- * @typedef {{
- *  tag?: keyof HTMLElementTagNameMap,
- *  children?: import('@style-guide/helpers/AddChildren').ChildrenParamType,
- *  className?: string,
- *  fullWidth?: boolean,
- *  [x: string]: *
- * }} CreateElementPropertiesType
- */
-/**
- * @template {keyof HTMLElementTagNameMap} T
- * @param {{tag?: T | "div"} & CreateElementPropertiesType} param0
- */
-export default function CreateElement({
-  tag = "div",
+import classnames from "classnames";
+import AddChildren from "@style-guide/helpers/AddChildren";
+import SetProps from "@style-guide/helpers/SetProps";
+import type { ChildrenParamType } from "@style-guide/helpers/AddChildren";
+
+type CreateElementPropsType<T> = {
+  tag: T,
+  children?: ChildrenParamType,
+  className?: string,
+  fullWidth?: boolean,
+  onClick?: Event => mixed,
+  ...
+};
+
+export default function CreateElement<T: string>({
+  tag,
   children,
   className,
   fullWidth,
+  onClick,
   ...props
-}) {
-  /**
-   * @type {HTMLElementTagNameMap[T]}
-   */
-  let element = (document.createElement(tag));
-  let classNames = classnames(className, {
+}: CreateElementPropsType<T>) /* : $ElementType<HTMLElementTagNameMap3, T> */ {
+  if (tag === null || tag === undefined) throw Error("Tag name is required");
+
+  const classNames = classnames(className, {
     "sg--full": fullWidth,
   });
 
-  if (classNames)
-    element.className = classNames;
+  const element = document.createElement(tag);
+  element.className = classNames;
 
   AddChildren(element, children);
   SetProps(element, props);
+
+  if (onClick && typeof onClick === "function")
+    element.addEventListener("click", onClick);
 
   return element;
 }

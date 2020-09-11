f17d177565abebbc454275f9053511eb0377aa7f
seahindeniz <seahindeniz@gmail.com>
2020-09-11 21:03:32 +0300

    Simplify

--- src/helpers/HideElement.ts
+++ src/helpers/HideElement.ts
@@ -2,10 +2,8 @@ export default (element: HTMLElement | JQuery) => {
   if (!element) return;
 
   if (element instanceof HTMLElement) {
-    if (element && element.parentElement) {
-      element.parentElement.removeChild(element);
-    }
-  } else if (element.detach) {
-    element.detach();
+    element?.parentElement?.removeChild(element);
+  } else {
+    element?.detach();
   }
 };

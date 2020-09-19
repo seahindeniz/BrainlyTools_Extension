84907852dfc1f0e8977e9eb3e1639414ca53b718
seahindeniz <seahindeniz@gmail.com>
2020-09-19 18:08:56 +0300

    Change label icon

--- src/scripts/views/12-QuestionAdd/_/ReportContents/Queue/Filter/ContentLength.ts
+++ src/scripts/views/12-QuestionAdd/_/ReportContents/Queue/Filter/ContentLength.ts
@@ -64,7 +64,7 @@ export default class ContentLength {
         color: "gray",
         onClose: this.HideLabel.bind(this),
         icon: new Icon({
-          type: "equation",
+          type: "ext-measuring-tape",
         }),
         children: [
           `${

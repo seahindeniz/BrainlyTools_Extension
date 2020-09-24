2f57ba9199f5e85cfc8b524a4d52628b77dc4e5b
seahindeniz <seahindeniz@gmail.com>
2020-09-25 01:20:40 +0300

    Add onTouchStart prop

--- src/components/style-guide/helpers/SetProps.ts
+++ src/components/style-guide/helpers/SetProps.ts
@@ -7,6 +7,7 @@ export type CommonComponentPropsType = {
   onClick?: MouseEventHandler | MouseEventHandler[];
   onMouseEnter?: MouseEventHandler | MouseEventHandler[];
   onMouseLeave?: MouseEventHandler | MouseEventHandler[];
+  onTouchStart?: TouchEvent | TouchEvent[];
   onInput?: EventHandler | EventHandler[];
   style?: Partial<CSSStyleDeclaration>;
 };

f66537ee675fc8265ea0cb820484048715f3470d
seahindeniz <seahindeniz@gmail.com>
2020-11-03 01:14:24 +0300

    Rename class name

--- src/scripts/views/12-QuestionAdd/_/ReportContents/Queue/Filter/User/Reporter.ts
+++ src/scripts/views/12-QuestionAdd/_/ReportContents/Queue/Filter/User/Reporter.ts
@@ -1,7 +1,7 @@
 import User from "./User";
 import type QueueClassType from "../../Queue";
 
-export default class Reported extends User {
+export default class Reporter extends User {
   constructor(main: QueueClassType) {
     super(main, "reporter", "peach");
   }

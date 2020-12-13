729892c8089ae59ebf4a2e071a391a20c34090f3
seahindeniz <seahindeniz@gmail.com>
2020-12-13 20:09:10 +0300

    Refactor popup notification

--- src/popup/components/notification.ts
+++ src/popup/components/notification.ts
@@ -1,22 +1,36 @@
 /**
- * - type:
- *  - primary = $turquoise
- *  - link = $blue
- *  - info = $cyan
- *  - success = $green
- *  - warning = $yellow
- *  - danger = $red
+ * - primary = turquoise
+ * - link = blue
+ * - info = cyan
+ * - success = green
+ * - warning = yellow
+ * - danger = red
  */
-export default function PopupNotification(text: string, type = "success") {
-  const $notify = $(`<div class="notification is-${type}">${text}</div>`);
+
+type NotificationTypeType =
+  | "primary"
+  | "link"
+  | "info"
+  | "success"
+  | "warning"
+  | "danger";
+
+export default function PopupNotification(
+  text: string,
+  type: NotificationTypeType = "success",
+  sticky?: boolean,
+) {
+  const $notify = $(
+    `<div class="notification is-${type}" has-text-weight-bold>${text}</div>`,
+  );
 
   $notify.prependTo(".notification-list");
-  setTimeout(() => $notify.slideUp(), 3000);
 
-  $notify.on("click", function () {
-    $(this).slideUp("normal", function () {
-      this.remove();
-    });
-  });
+  if (!sticky) {
+    setTimeout(() => $notify.slideUp(), 3000);
+  }
+
+  $notify.on("click", () => $notify.slideUp("normal", () => $notify.remove()));
+
   return true;
 }

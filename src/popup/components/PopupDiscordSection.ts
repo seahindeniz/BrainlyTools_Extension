5f8992ab7999ae7e4833a07eba608b7a10a0a2b8
seahindeniz <seahindeniz@gmail.com>
2020-12-18 18:32:29 +0300

    Fix Discord widget

--- src/popup/components/PopupDiscordSection.ts
+++ src/popup/components/PopupDiscordSection.ts
@@ -1,18 +1,20 @@
 import CreateElement from "@components/CreateElement";
+import Build from "@root/helpers/Build";
+import { Flex, Text } from "@style-guide";
 
 export default class PopupDiscordSection {
-  container: HTMLDivElement;
+  container: import("@style-guide/Flex").FlexElementType;
 
   constructor() {
     this.Render();
   }
 
   Render() {
-    this.container = CreateElement({
-      tag: "div",
-      id: "discord-banner",
-      children: [
+    this.container = Build(Flex({ direction: "column" }), [
+      [
+        Flex({ grow: true, justifyContent: "center", marginTop: "l" }),
         CreateElement({
+          id: "discord-widget",
           tag: "iframe",
           src: `https://discord.com/widget?id=${System.data.Brainly.userData.extension.discordServer}&theme=light`,
           width: "90%",
@@ -23,6 +25,16 @@ export default class PopupDiscordSection {
             "allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts",
         }),
       ],
-    });
+      [
+        Flex({ marginTop: "s", marginRight: "l", justifyContent: "flex-end" }),
+        Text({
+          tag: "a",
+          color: "blue-dark",
+          target: "_blank",
+          href: "http://bit.ly/3my8Q6i",
+          children: `ℹ ${System.data.locale.core.discordPopup.informationLine} 😗`,
+        }),
+      ],
+    ]);
   }
 }

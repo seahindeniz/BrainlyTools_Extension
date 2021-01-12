12acd00e9b41c2c8ed5a280f7befbd14d85ea684
seahindeniz <seahindeniz@gmail.com>
2021-01-12 15:39:39 +0300

    Replace gulp-sass with gulp-dart-sass

--- scripts/gulp/scss.js
+++ scripts/gulp/scss.js
@@ -1,35 +1,27 @@
 import { dest, src } from "gulp";
 import gulpPlumber from "gulp-plumber";
-import gulpSass from "gulp-sass";
-// import gulpSourceMaps from "gulp-sourcemaps";
 import magicImporter from "node-sass-magic-importer";
-import sass from "sass";
-
-gulpSass.compiler = sass;
+import gulpSass from "gulp-dart-sass";
 
 function compileScssFiles(files, destPath) {
-  return (
-    src(files)
-      .pipe(gulpPlumber())
-      // .pipe(gulpSourceMaps.init())
-      .pipe(
-        gulpSass
-          .sync({
-            outputStyle:
-              process.env.NODE_ENV === "production" ? "compressed" : "expanded",
-            precision: 10,
-            includePaths: ["node_modules", "."],
-            importer: [magicImporter()],
-          })
-          .on("error", gulpSass.logError),
-      )
-      // .pipe(gulpSourceMaps.write(`./`))
-      .pipe(dest(destPath, { overwrite: true }))
-  );
+  return src(files)
+    .pipe(gulpPlumber())
+    .pipe(
+      gulpSass
+        .sync({
+          outputStyle:
+            process.env.NODE_ENV === "production" ? "compressed" : "expanded",
+          precision: 10,
+          includePaths: ["node_modules", "."],
+          importer: [magicImporter()],
+        })
+        .on("error", gulpSass.logError),
+    )
+    .pipe(dest(destPath, { overwrite: true }));
 }
 
-export default () => {
-  return compileScssFiles(
+export default () =>
+  compileScssFiles(
     [
       "src/styles/**/*.scss",
       "src/styles/**/**/*.scss",
@@ -37,4 +29,3 @@ export default () => {
     ],
     `${process.env.BUILD_FOLDER}/styles`,
   );
-};

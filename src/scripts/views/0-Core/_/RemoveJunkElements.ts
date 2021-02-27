5bfebe1905e4caea0c82aa721ce2082971b7a9a1
seahindeniz <seahindeniz@gmail.com>
2021-02-27 13:57:45 +0300

    Remove ad boxes

--- src/scripts/views/0-Core/_/RemoveJunkElements.ts
+++ src/scripts/views/0-Core/_/RemoveJunkElements.ts
@@ -1,11 +1,11 @@
 import WaitForElement from "@root/helpers/WaitForElement";
 
 const querySelectors = [
-  //
   ".js-web-to-app-banner",
   ".js-mobile-add-question",
   `[class*="BrainlyPlusToaster__section-"]`,
   ".brn-qpage-next-banner",
+  ".brn-ads-box",
 ];
 
 export default function RemoveJunkElements() {
